b0edabfcb7924680c3c7f40ebd3059e356ba33c6
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-25 01:42:38 +0100

    qf

--- Projekt/setupPriorytet.sql
+++ Projekt/setupPriorytet.sql
@@ -2,4 +2,4 @@ INSERT INTO priorytet (id, opis) VALUES (1, 'Meh');
 INSERT INTO priorytet (id, opis) VALUES (2, 'Moze kiedys');
 INSERT INTO priorytet (id, opis) VALUES (3, 'Kiedys obejrze');
 INSERT INTO priorytet (id, opis) VALUES (4, 'Musze obejrzec');
-INSERT INTO priorytet (id, opis) VALUES (5, 'Oglądam dzisiaj!');
+INSERT INTO priorytet (id, opis) VALUES (5, 'Ogladam dzisiaj!');
