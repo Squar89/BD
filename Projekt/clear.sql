a90cbf9aa178bee7d264082a635640e7f1441bf8
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-25 01:29:22 +0100

    Wrote setup Gatunek and Priorytet

---
+++ Projekt/clear.sql
@@ -0,0 +1,7 @@
+DROP TABLE film;
+DROP TABLE gatunek;
+DROP TABLE ocena_filmu;
+DROP TABLE pozycja;
+DROP TABLE priorytet;
+DROP TABLE rezyser;
+DROP TABLE uzytkownik;
---
+++ Projekt/setupPriorytet.sql
@@ -0,0 +1,5 @@
+INSERT INTO priorytet (id, opis) VALUES (1, 'Meh');
+INSERT INTO priorytet (id, opis) VALUES (2, 'Moze kiedys');
+INSERT INTO priorytet (id, opis) VALUES (3, 'Kiedys obejrze');
+INSERT INTO priorytet (id, opis) VALUES (4, 'Musze obejrzec');
+INSERT INTO priorytet (id, opis) VALUES (5, 'Oglądam dzisiaj!');
