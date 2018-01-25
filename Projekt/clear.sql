72708557a20e9161d62dff7f8025bd7e0c5a80fd
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-25 01:36:27 +0100

    Fixed v2

--- Projekt/clear.sql
+++ Projekt/clear.sql
@@ -1,8 +1,7 @@
-DROP TABLE gatunek;
-DROP TABLE priorytet;
-DROP TABLE rezyser;
-
-DROP TABLE film;
 DROP TABLE ocena_filmu;
 DROP TABLE pozycja;
 DROP TABLE uzytkownik;
+DROP TABLE priorytet;
+DROP TABLE film;
+DROP TABLE rezyser;
+DROP TABLE gatunek;
