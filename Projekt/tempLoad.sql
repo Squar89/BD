6f52360eb24b9c6c78fde81cb3f0cd8a08146453
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-25 03:50:37 +0100

    Some values to uzytkownik, lista and ocena

---
+++ Projekt/tempLoad.sql
@@ -0,0 +1,6 @@
+INSERT INTO uzytkownik (id, nazwa_uzytkownika) VALUES (1, Squar);
+
+INSERT INTO pozycja (pozycja, film_id, priorytet_id, uzytkownik_id) VALUES (1, 357, 3, 1);
+INSERT INTO pozycja (pozycja, film_id, priorytet_id, uzytkownik_id) VALUES (2, 1246, 1, 1);
+
+INSERT INTO ocena_filmu (id, opinia, film_id, uzytkownik_id) VALUES (1, '5/7', 4112, 1);
