0b8ce76bacfe7585521ea09deef853ccc25f894f
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-26 23:33:25 +0100

    updated clear

--- Projekt/clear.sql
+++ Projekt/clear.sql
@@ -7,3 +7,9 @@ DROP TABLE rezyser;
 DROP TABLE gatunek;
 
 DROP VIEW lista_do_obejrzenia;
+
+DROP SEQUENCE id_uzytkownik_s;
+DROP SEQUENCE id_ocena_s;
+
+DROP TRIGGER id_uzytkownik_trigger;
+DROP TRIGGER id_ocena_trigger;
--- Projekt/createView.sql
+++ Projekt/createView.sql
@@ -2,11 +2,11 @@ CREATE VIEW lista_do_obejrzenia (pozycja, tytul, rok_produkcji, gatunek, rezyser
 SELECT poz.pozycja, f.tytul, f.rok_produkcji, g.nazwa, r.imie, r.nazwisko, f.czas_trwania, f.srednia_ocen, pr.opis, poz.uzytkownik_id
 FROM pozycja poz LEFT JOIN film f ON poz.film_id = f.id LEFT JOIN priorytet pr ON poz.priorytet_id = pr.id LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id LEFT JOIN uzytkownik u ON poz.uzytkownik_id = u.id;
 
-CREATE OR REPLACE SEQUENCE id_uzytkownik_s
+CREATE SEQUENCE id_uzytkownik_s
 START WITH 1
 INCREMENT BY 1;
 
-CREATE OR REPLACE SEQUENCE id_ocena_s
+CREATE SEQUENCE id_ocena_s
 START WITH 1
 INCREMENT BY 1;
 
