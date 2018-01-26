4452a1b1cf4c3bdaff5c7692aee95fa92b5e723a
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-26 23:29:28 +0100

    Added or replace

--- Projekt/createView.sql
+++ Projekt/createView.sql
@@ -2,28 +2,26 @@ CREATE VIEW lista_do_obejrzenia (pozycja, tytul, rok_produkcji, gatunek, rezyser
 SELECT poz.pozycja, f.tytul, f.rok_produkcji, g.nazwa, r.imie, r.nazwisko, f.czas_trwania, f.srednia_ocen, pr.opis, poz.uzytkownik_id
 FROM pozycja poz LEFT JOIN film f ON poz.film_id = f.id LEFT JOIN priorytet pr ON poz.priorytet_id = pr.id LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id LEFT JOIN uzytkownik u ON poz.uzytkownik_id = u.id;
 
-ALTER VIEW lista_do_obejrzenia ADD CONSTRAINT no_duplicates UNIQUE ( tytul, uzytkownik_id );
-
-CREATE SEQUENCE id_nowy_uzytkownik
+CREATE OR REPLACE SEQUENCE id_uzytkownik_s
 START WITH 1
 INCREMENT BY 1;
 
-CREATE SEQUENCE id_ocena
+CREATE OR REPLACE SEQUENCE id_ocena_s
 START WITH 1
 INCREMENT BY 1;
 
-CREATE TRIGGER id_uzytkownik_trigger
+CREATE OR REPLACE TRIGGER id_uzytkownik_trigger
 BEFORE INSERT ON osoba
 FOR EACH ROW
 BEGIN
-  SELECT id_nowy_uzytkownik_seq.nextval INTO :NEW.id FROM dual;
+  SELECT id_uzytkownik_s_seq.nextval INTO :NEW.id FROM dual;
 END;
 /
 
-CREATE TRIGGER id_ocena_trigger
+CREATE OR REPLACE TRIGGER id_ocena_trigger
 BEFORE INSERT ON ocena_filmu
 FOR EACH ROW
 BEGIN
-  SELECT id_ocena_seq.nextval INTO :NEW.id FROM dual;
+  SELECT id_ocena_s_seq.nextval INTO :NEW.id FROM dual;
 END;
 /
