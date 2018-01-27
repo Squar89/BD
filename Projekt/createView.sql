721a914c9863010aa7d3784f7c1bb06e3b844b49
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-27 20:57:33 +0100

    Fix?

--- Projekt/createView.sql
+++ Projekt/createView.sql
@@ -35,8 +35,8 @@ BEGIN
   IF :NEW.ocena IS NOT NULL THEN
     UPDATE film
       SET film.liczba_ocen = (SELECT liczba_ocen + incr FROM film)
-      SET film.srednia_ocen = film.srednia_ocen + ((:NEW.ocena - film.srednia_ocen) / film.liczba_ocen)
-    WHERE film.id = :NEW.film_id;
+          film.srednia_ocen = film.srednia_ocen + ((:NEW.ocena - film.srednia_ocen) / film.liczba_ocen)
+      WHERE film.id = :NEW.film_id;
   END IF;
 END;
 /
