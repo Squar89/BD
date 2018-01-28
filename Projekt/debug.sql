fdd889f4ba0d9d7d0f47074a5aa7334daefd84d6
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-28 13:26:44 +0100

    Debug time

--- Projekt/debug.sql
+++ Projekt/debug.sql
@@ -1,12 +1,10 @@
 CREATE OR REPLACE TRIGGER update_rating
 AFTER INSERT ON ocena_filmu
 FOR EACH ROW
-DECLARE
-  incr int := 1;
 BEGIN
   IF :NEW.ocena IS NOT NULL THEN
     UPDATE film
-      SET film.liczba_ocen = (SELECT liczba_ocen + incr FROM film)
+      SET film.liczba_ocen = (SELECT liczba_ocen + 1 FROM film)
           film.srednia_ocen = film.srednia_ocen + ((:NEW.ocena - film.srednia_ocen) / film.liczba_ocen)
       WHERE film.id = :NEW.film_id;
   END IF;
