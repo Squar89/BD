2e5032c91b93d5dc1e00bd480a7453d922fcc3c9
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-28 14:24:59 +0100

    Updating all instead of one row

--- Projekt/debug.sql
+++ Projekt/debug.sql
@@ -3,7 +3,7 @@ AFTER INSERT ON ocena_filmu
 FOR EACH ROW
 BEGIN
   IF :NEW.ocena IS NOT NULL THEN
-    UPDATE film SET film.liczba_ocen = (SELECT liczba_ocen + 1 FROM film WHERE film.id = :NEW.film_id);
+    UPDATE film SET film.liczba_ocen = film.liczba_ocen + 1 WHERE film.id = :NEW.film_id;
     UPDATE film SET film.srednia_ocen = film.srednia_ocen + ((:NEW.ocena - film.srednia_ocen) / film.liczba_ocen) WHERE film.id = :NEW.film_id;
   END IF;
 END;
