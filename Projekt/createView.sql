13bfe8e6c6e2005f8f582212292a7e32c951cea8
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-28 13:23:02 +0100

    Moved stuff around

--- Projekt/createView.sql
+++ Projekt/createView.sql
@@ -1,31 +1,3 @@
-CREATE VIEW lista_do_obejrzenia (pozycja, tytul, rok_produkcji, gatunek, rezyser_imie, rezyser_nazwisko, czas_trwania, srednia_ocen, priorytet, uzytkownik_id) AS
-SELECT poz.pozycja, f.tytul, f.rok_produkcji, g.nazwa, r.imie, r.nazwisko, f.czas_trwania, f.srednia_ocen, pr.opis, poz.uzytkownik_id
-FROM pozycja poz LEFT JOIN film f ON poz.film_id = f.id LEFT JOIN priorytet pr ON poz.priorytet_id = pr.id LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id LEFT JOIN uzytkownik u ON poz.uzytkownik_id = u.id;
-
-CREATE SEQUENCE id_uzytkownik_seq
-START WITH 1
-INCREMENT BY 1;
-
-CREATE SEQUENCE id_ocena_seq
-START WITH 1
-INCREMENT BY 1;
-
-CREATE OR REPLACE TRIGGER id_uzytkownik_trigger
-BEFORE INSERT ON uzytkownik
-FOR EACH ROW
-BEGIN
-  SELECT id_uzytkownik_seq.nextval INTO :NEW.id FROM dual;
-END;
-/
-
-CREATE OR REPLACE TRIGGER id_ocena_trigger
-BEFORE INSERT ON ocena_filmu
-FOR EACH ROW
-BEGIN
-  SELECT id_ocena_seq.nextval INTO :NEW.id FROM dual;
-END;
-/
-
 CREATE OR REPLACE TRIGGER update_rating
 AFTER INSERT ON ocena_filmu
 FOR EACH ROW
