4301504b5bd7d3b9dc913510b84dff3b764ad719
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-28 14:36:15 +0100

    Another cast

--- Projekt/debug.sql
+++ Projekt/debug.sql
@@ -4,7 +4,8 @@ FOR EACH ROW
 BEGIN
   IF :NEW.ocena IS NOT NULL THEN
     UPDATE film SET film.liczba_ocen = film.liczba_ocen + 1 WHERE film.id = :NEW.film_id;
-    UPDATE film SET film.srednia_ocen = film.srednia_ocen + ((:NEW.ocena - film.srednia_ocen) / CAST(film.liczba_ocen AS number(*, 1))) WHERE film.id = :NEW.film_id;
+    UPDATE film SET film.srednia_ocen = film.srednia_ocen + ((CAST(:NEW.ocena AS number(*, 2)) - film.srednia_ocen) / CAST(film.liczba_ocen AS number(*, 2)))
+                WHERE film.id = :NEW.film_id;
   END IF;
 END;
 /
