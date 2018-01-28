b3ffb18cce52e94f889559a74e07d5bc2c692464
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-28 18:05:32 +0100

    Simple authentication

---
+++ Projekt/auth.php
@@ -0,0 +1,8 @@
+<?php
+if ($_POST['login']=="jw386401" AND $_POST['haslo']=="qwerty") {
+    echo "Zostales poprawnie zalogowany!<br>";
+}
+else {
+    echo "Odmowa dostepu.<br>";
+}
+?>
---
+++ Projekt/login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta http-equiv='Content-type' content='text/html; charset=utf-8'>
+<title>Logowanie</title>
+</head>
+<body>
+<h1>Logowanie</h1>
+<form action='auth.php' method='post'>
+Login: <input type='text' name='login' /><br>
+Hasło: <input type='password' name='haslo' /><br>
+<input type='Submit' value='Zaloguj'>
+</form>
+</body>
+</html>
