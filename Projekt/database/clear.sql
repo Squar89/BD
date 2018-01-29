132e91272024bb163c663ce228a98e1d0e649d02
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-29 18:44:41 +0100

    qf

--- Projekt/database/clear.sql
+++ Projekt/database/clear.sql
@@ -1,5 +1,6 @@
 DROP TRIGGER id_uzytkownik_trigger;
 DROP TRIGGER id_ocena_trigger;
+DROP TRIGGER id_pozycja_trigger;
 DROP TRIGGER update_rating;
 
 DROP TABLE ocena_filmu;
@@ -14,3 +15,4 @@ DROP VIEW lista_do_obejrzenia;
 
 DROP SEQUENCE id_uzytkownik_seq;
 DROP SEQUENCE id_ocena_seq;
+DROP SEQUENCE if_pozycja_seq;
