e4ac764d0dfdee5a76a5b14417a2f75051f3bdfa
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-30 22:13:04 +0100

    Added index for film

--- Projekt/database/optimize.sql
+++ Projekt/database/optimize.sql
@@ -1,3 +1,6 @@
+CREATE INDEX film_index ON film(rok_produkcji, czas_trwania, srednia_ocen);
+
 analyze table film compute statistics;
 analyze table uzytkownik compute statistics;
 analyze table pozycja compute statistics;
+analyze index film_index compute statistics;
