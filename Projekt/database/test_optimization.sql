a693cb460b3b9a94f1d437934dc8fd0eb17f0656
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-30 23:01:12 +0100

    Test opt2

--- Projekt/database/test_optimization.sql
+++ Projekt/database/test_optimization.sql
@@ -1,10 +1,11 @@
 EXPLAIN PLAN FOR SELECT f.id AS ID, f.tytul AS TYTUL, f.rok_produkcji AS ROK_PRODUKCJI, g.nazwa AS GATUNEK, r.imie AS IMIE_REZYSERA,
                         r.nazwisko AS NAZWISKO_REZYSERA, f.czas_trwania AS CZAS_TRWANIA, f.srednia_ocen AS OCENA
-                 FROM film f LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id
+                 FROM film f LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id;
 SELECT * FROM TABLE(dbms_xplan.display);
 
 @optimize.sql
 
 EXPLAIN PLAN FOR SELECT f.id AS ID, f.tytul AS TYTUL, f.rok_produkcji AS ROK_PRODUKCJI, g.nazwa AS GATUNEK, r.imie AS IMIE_REZYSERA,
                         r.nazwisko AS NAZWISKO_REZYSERA, f.czas_trwania AS CZAS_TRWANIA, f.srednia_ocen AS OCENA
-                 FROM film f LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id
+                 FROM film f LEFT JOIN gatunek g ON f.gatunek_id = g.id LEFT JOIN rezyser r ON f.rezyser_id = r.id;
+SELECT * FROM TABLE(dbms_xplan.display);
