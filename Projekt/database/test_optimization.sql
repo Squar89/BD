ffb7c97ee3ed830d1e2552b5496bccc4a74dcc34
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-30 22:38:25 +0100

    ..

--- Projekt/database/test_optimization.sql
+++ Projekt/database/test_optimization.sql
@@ -1,7 +1,7 @@
-EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
+EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film LEFT JOIN gatunek ON film.gatunek_id = gatunek.id LEFT JOIN rezyser ON film.rezyser_id = rezyser.id WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
 SELECT * FROM TABLE(dbms_xplan.display);
 
 @optimize.sql
 
-EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
+EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film LEFT JOIN gatunek ON film.gatunek_id = gatunek.id LEFT JOIN rezyser ON film.rezyser_id = rezyser.id WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
 SELECT * FROM TABLE(dbms_xplan.display);
