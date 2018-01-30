680765dbf221e4453437686f7a0612ffd1887813
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-30 21:55:50 +0100

    qf

--- Projekt/database/optimize.sql
+++ Projekt/database/optimize.sql
@@ -1,6 +1,3 @@
-CREATE INDEX uzytkownik_name ON uzytkownik(nazwa_uzytkownika);
-
 analyze table film compute statistics;
 analyze table uzytkownik compute statistics;
 analyze table pozycja compute statistics;
-analyze index uzytkownik_name compute statistics;
