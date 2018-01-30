5071d7c36f83f8be838db8b22807a60540e6a568
Jakub Wróblewski <j.wroblewski97@gmail.com>
2018-01-30 22:35:07 +0100

    .

--- Projekt/database/test_optimization.sql
+++ Projekt/database/test_optimization.sql
@@ -1,7 +1,7 @@
-EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 7 AND rok_produkcji < 2005 AND czas_trwania < 180;
+EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
 SELECT * FROM TABLE(dbms_xplan.display);
 
 @optimize.sql
 
-EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 7 AND rok_produkcji < 2005 AND czas_trwania < 180;
+EXPLAIN PLAN FOR SELECT srednia_ocen, tytul, rok_produkcji, czas_trwania FROM film WHERE srednia_ocen > 6.1 AND rok_produkcji < 2010 AND czas_trwania < 180;
 SELECT * FROM TABLE(dbms_xplan.display);
